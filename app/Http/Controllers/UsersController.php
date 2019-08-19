3f294170a89bd0563199582613a67cc01612bf6b
Jacob Cheng <im.jaocbz@outlook.com>
2019-08-19 05:10:38 +0000

    管理员可以删除用户

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -78,4 +78,12 @@ class UsersController extends Controller
     {
         return view('users.show', compact('user'));
     }
+
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
+    }
 }
